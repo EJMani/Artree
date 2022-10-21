17201adbb09cfe4bc1375c135333f8484034f071
John Meyers <john@johnmeye.rs>
2022-10-21 14:29:44 -0500

    added fonts

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import logo from './artree_logo_transparent_longer_stem.png'
-import font from './artree_title.png'
+import { useEffect, useCallback } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'newake': require('./assets/newake-demo-400.otf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source={logo} style={{ width: 400, height: 400 }} />
-      <Image source={font} style={{ width: 400, height: 100 }} />
-    </View>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <Text style={{ fontFamily: 'newake', fontSize: 30, color: '#1FCEC6' }}>artree</Text>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1f1e49',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1f1e49"
   },
 });
