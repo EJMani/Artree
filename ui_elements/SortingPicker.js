1f142163811f3b6205e22099c5a74c0bfe1a767f
Nick-LeBlanc <97707415+Nick-LeBlanc@users.noreply.github.com>
2022-11-21 19:15:42 -0600

    Fixed Picker for ios

--- ui_elements/SortingPicker.js
+++ ui_elements/SortingPicker.js
@@ -1,20 +1,25 @@
-import React, {Component, useCallback} from 'react';
-import { View, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import { View, StyleSheet, Platform} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from "expo-splash-screen";
 
 class SortingPicker extends Component {
 
-    state = {user: ''}
+    state = {user: "popular"}
     updateUser = (user) => {
         this.setState({ user: user })
     }
 
+    constructor(props){
+        super(props);
+        Platform.OS === "android"? this.bottom=10 : this.bottom=95;
+    }    
+
     render() {
         return (
             <View style={{borderRadius: 25, height: 30, overflow: 'hidden', translateX: -10,}}>
-                <Picker selectedValue={this.state.user} onValueChange={this.updateUser} style={styles.picker}>
+                <Picker selectedValue={this.state.user} onValueChange={this.updateUser} style={[styles.picker,{bottom:this.bottom}]}>
                     <Picker.Item style={{fontSize: 14, fontFamily: 'inter',}} label="Popular" value="popular"/>
                     <Picker.Item style={{fontSize: 14, fontFamily: 'inter',}} label="Newest" value="newest"/>
                 </Picker>
@@ -29,6 +34,5 @@ const styles = StyleSheet.create({
         overflow: "hidden",
         backgroundColor: '#fff',
         width: 115,
-        bottom: 10,
     },
 })
