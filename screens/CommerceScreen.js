e45f685e0157f0f8f0ed002b9f0310cd64f753e3
John Meyers <john@johnmeye.rs>
2022-11-26 17:51:05 -0600

    Tabbed menu added to CommerceScreen.js

--- screens/CommerceScreen.js
+++ screens/CommerceScreen.js
@@ -1,14 +1,61 @@
 import {StatusBar} from 'expo-status-bar';
-import {Text, StyleSheet, Button, View} from 'react-native';
-import {NavigationContainer} from "@react-navigation/native";
+import {Text, StyleSheet, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+function CommissionsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#1f1e49" }}>
+            <Text>Feed!</Text>
+        </View>
+    );
+}
+
+function AuctionsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#1f1e49", }}>
+            <Text>Notifications!</Text>
+        </View>
+    );
+}
+
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+    return (
+        <Tab.Navigator
+            initialRouteName="Feed"
+            screenOptions={{
+                tabBarActiveTintColor: '#1FCEC6',
+                tabBarInactiveTintColor: '#fff',
+                tabBarLabelStyle: { fontSize: 16 , fontFamily: 'inter'},
+                tabBarStyle: { backgroundColor: '#383CF4' },
+            }}
+        >
+            <Tab.Screen
+                name="Feed"
+                component={CommissionsScreen}
+                options={{ tabBarLabel: 'Commissions' }}
+            />
+            <Tab.Screen
+                name="Notifications"
+                component={AuctionsScreen}
+                options={{ tabBarLabel: 'Auctions' }}
+            />
+        </Tab.Navigator>
+    );
+}
 
 export default function CommerceScreen({navigation}) {
+
     return (
-        <View style={styles.container}>
+        <NavigationContainer independent={true} style={styles.container}>
+            <MyTabs />
             <StatusBar
                 style="light" //this took me an hour to figure out :(
             />
-        </View>
+        </NavigationContainer>
     );
 }
 
