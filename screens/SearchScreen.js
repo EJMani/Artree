fa99160af0fe23348dc96a9199939c291e82c3cf
Nick-LeBlanc <97707415+Nick-LeBlanc@users.noreply.github.com>
2022-11-27 15:13:54 -0600

    basic search w/ backend

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,13 +1,65 @@
+import { useState,useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, StyleSheet, Button, View } from "react-native";
+import { SearchBar } from "react-native-elements";
+import { useQueryClient,useQuery } from "@tanstack/react-query";
+
 
 export default function SearchScreen({ navigation }) {
+  let [search, updateSearch] = useState("");
+  const QueryClient = useQueryClient();
+
+  const { 
+    isLoading,
+    isError,
+    data,
+    error,
+    hasNextPage,
+    fetchNextPage,
+    refetch,
+    isFetching,
+    isFetchingNextPage,} = useQuery({ 
+    queryKey: ['search'], 
+    queryFn: async ()=>{
+        const res  = await fetch(`http://54.236.91.239:3000/searchArt/${search}`,{
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        });
+        const data = res.json();
+        return data;
+    },
+    refetchOnWindowFocus:false,
+    enabled:false
+  })
+
+  useEffect(()=>{
+    if(search!== "")
+      refetch();
+    if(search!== "" || search!== " ")
+      QueryClient.invalidateQueries(['search']);
+  },[search]);
+
   return (
+    
     <View style={styles.container}>
+      <SearchBar
+        style={styles.SearchBar} 
+        placeholder="Type Here..."
+        onChangeText={updateSearch}
+        value={search} 
+      />
+      <View style={styles.Results}>
+        <Text style={{color: "white"}}>
+         {data !== undefined? data.map((post, key)=>(
+            <Text key={key}>{post.title} - </Text>
+          )): <></>}
+          </Text>
+      </View>
       <StatusBar
         style="light" //this took me an hour to figure out :(
       />
-      <Text>search screen bitches!</Text>
     </View>
   );
 }
@@ -15,8 +67,12 @@ export default function SearchScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-around",
-    alignItems: "center",
     backgroundColor: "#1f1e49",
   },
+  SearchBar:{
+    width:'100%'
+  },
+  Results:{
+    alignItems: "center",
+  }
 });
